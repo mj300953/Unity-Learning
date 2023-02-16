cc3e2bb3ffffe96fc581c423efc722832d56c16d
Adam Janikowski <adam.janikowski99@gmail.com>
2023-02-16 20:46:37 +0100

    Imported TMP Pro, added some examples

---
+++ Assets/Scripts/DataTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DataTest : MonoBehaviour
+{
+    [SerializeField] private ApplicationData data;
+
+    private void Start()
+    {
+        Debug.Log(data.Value);
+    }
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Button loadLevelButton;
+    [SerializeField] private string levelName;
+
+    private void Awake()
+    {
+        loadLevelButton.onClick.AddListener(LoadLevel);
+    }
+
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
