06316ad7dd4d2e71782521bbe457faab81b37ab9
Mateusz Janikowski <mateuszjanikowski02@gmail.com>
2023-02-18 18:57:09 +0100

    a

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,22 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Health))]
-public class Enemy : MonoBehaviour
+public class Enemy : Damageable
 { 
-    private Health health;
-
-    private void Awake()
-    {
-        health = GetComponent<Health>();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Attack")
-        {
-            health.TakeDamage(10);
-        }
-    }
+   
 }
